0b3d8e377f515749127e025899d355c70b3309a0
Rameshver <rameshver091299@gmail.com>
2022-10-14 20:51:51 +0530

    modified port

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/", router);
-app.listen(5000, () => console.log("Server Running"));
+const PORT = process.env.PORT || 3030;
+
+app.listen(PORT, () => console.log("Server Running"));
 
 const contactEmail = nodemailer.createTransport({
     service: 'gmail',
